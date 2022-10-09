60a2d0d36b1591afa686218e012c89ceed87fc66
Bhargav Sarvaria <34936854+bhargav-sarvaria@users.noreply.github.com>
2022-10-09 02:52:03 -0700

    Updated App Js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,12 @@ import { CSSTransition } from 'react-transition-group';
 function App() {
   return (
     <div className="App">
-
-      <Dropdown
-        label={'Animals'}
-        allOption={true}
-        initialSelectedOptions={['Dog']}
-        multiSelect={true}
-        initialOptions={['Cat', 'Dog', 'Lion', 'Wolf', 'Zeebra']} />
-
       <Dropdown
         label={'Colors'}
         allOption={true}
         initialSelectedOptions={['Blue']}
         multiSelect={true}
         initialOptions={['White', 'Red', 'Blue', 'Brown', 'Black']} />
-
     </div>
   );
 }
